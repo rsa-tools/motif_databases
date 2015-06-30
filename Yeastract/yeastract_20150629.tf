2265886d74701c49fdaafd4950eb6106ea6f8be3
amedina <medina.alexiel@gmail.com>
2015-06-29 20:05:42 -0500

    added cisbp for s cerevisea and yeastract

---
+++ Yeastract/yeastract_20150629.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab7a319f7b5916b0720c1b19962e9c626ef425f42b08a42024adbcbdd0077332
+size 395683
---
+++ Yeastract/yeastract_20150629_relfreq.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:83b18a0fe6f1c7735dfc1e36f9b5d079c4bf0be421e67883be0c04c44365db6f
+size 395683
