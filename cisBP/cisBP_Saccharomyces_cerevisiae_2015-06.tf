d6a58fd270b1ec04c121a67fdf1f30ef5ef563ab
amedina <medina.alexiel@gmail.com>
2015-06-29 20:06:13 -0500

    added cisbp for cerevisiae

---
+++ cisBP/cisBP_Saccharomyces_cerevisiae_2015-06.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:672e18a05695f8778772016d53df70e150f67ef62c3345ef9711e66a702a7bb0
+size 520249
---
+++ cisBP/cisBP_Saccharomyces_cerevisiae_2015-06_relfreq.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f95494b45e628f0692fe3fbe771cd880f7ce4d7bfcaf5d66b7a37b3b40d008fb
+size 673727
