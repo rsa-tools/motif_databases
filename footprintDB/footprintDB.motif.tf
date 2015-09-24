bd8b8d7945f09acc8070470ade14075e483b9c15
RSAT admin at floresta plants.rsat.eu <bcontreras@eead.csic.es>
2015-09-24 12:22:14 +0200

    fixed bugs in Uniprobe matrices with frequencies

--- footprintDB/footprintDB.motif.tf
+++ footprintDB/footprintDB.motif.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7f9ca2d66a4e6eb2dcbc4bf7af9a065d1786f24f254fd932273d3226200351e
-size 4726454
+oid sha256:fd3820a7ad66f0b7aaf997fe753bb693a73e883b2a33e1de8d415adc913680db
+size 6198134
