238ca0d766a615941e94a272342fe8ec744446c4
Jaime CASTRO <jcastro@lcg.unam.mx>
2015-11-06 11:32:58 +0100

    New databases taken from FootprintDB

---
+++ Athamap/Athamap_2015-11-06.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:76515bd726afaca33ab08b507f2caee6dc5bb1b72c94d7dd776d8dc4042d6ef3
+size 75065
---
+++ DrosophilaTFs/DrosophilaTFs_2015-11-06.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1f71700f8d2668e2edcf1f7d5392174a4dd34b2babd4bdb7de96c51434e61985
+size 45776
