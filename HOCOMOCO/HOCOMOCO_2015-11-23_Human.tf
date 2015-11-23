b48ecc4a12d8db15d2bd28a6e35bd18ee753dbcf
Jaime CASTRO <jcastro@lcg.unam.mx>
2015-11-23 13:57:38 +0100

    New Hocomoco PSSMs for Human + Mouse

---
+++ HOCOMOCO/HOCOMOCO_2015-11-23_Human.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2e2a63f6d2e6dd64d97600b3faef425a8853de45a91044d15581081492b42eaf
+size 599112
---
+++ HOCOMOCO/HOCOMOCO_2015-11-23_Mouse.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:88650dfcde0be5e2e841a8cce2554cd78feb14fe5f8e798cab1e2db4f6b551a9
+size 386310
