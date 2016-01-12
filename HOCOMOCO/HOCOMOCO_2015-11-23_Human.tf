9b3d8740404ee940f37880ece7b7669dcba5d20d
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2016-01-12 11:21:13 +0100

    The motif IDs were renamed, now they include the motif quality given by Hocomooco authors

--- HOCOMOCO/HOCOMOCO_2015-11-23_Human.tf
+++ HOCOMOCO/HOCOMOCO_2015-11-23_Human.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e2a63f6d2e6dd64d97600b3faef425a8853de45a91044d15581081492b42eaf
-size 599112
+oid sha256:40d89a31b96b43f8cbfc1de35320e437379d84c0157d898649ff97150ca645be
+size 600394
