75da6482c7a71451ab96c4143949fb8f076ce4b3
RSAT admin at floresta plants.rsat.eu <bcontreras@eead.csic.es>
2016-02-08 10:40:25 +0100

    Updated footprintDB PWM files to have informative names. Also JASPAR is now on its last version.

--- footprintDB/footprintDB.motif.tf
+++ footprintDB/footprintDB.motif.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99ca4d02b95cd9a1d863f30d413a8e94e2adef06f46fc050890b25938db05350
-size 8964611
+oid sha256:da0adca198216df978aa34326e1c66c1b100b24a9e8b17641091deca5779439b
+size 9395799
--- footprintDB/footprintDB.plants.motif.tf
+++ footprintDB/footprintDB.plants.motif.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89f50251e2c0530e817d918b8efe9bfb4315d5f236d65b410d4f6f437e60171d
-size 890638
+oid sha256:a432a0060a5d1dc6902650d6dc900e02d3c1f6da5c2960f3adab150632855deb
+size 1060703
