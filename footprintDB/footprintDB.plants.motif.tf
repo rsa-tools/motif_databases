a8e1b7c495eb3955e3084bcb5a04d38527a24e02
Bruno Contreras <bconteras@eead.csic.es>
2016-06-03 09:51:06 +0200

    updated

--- footprintDB/footprintDB.plants.motif.tf
+++ footprintDB/footprintDB.plants.motif.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a432a0060a5d1dc6902650d6dc900e02d3c1f6da5c2960f3adab150632855deb
-size 1060703
+oid sha256:bc4e04a4c67f527beb7302e14f3b848ab51693bc17d1060d667032c97e1a740c
+size 1052110
