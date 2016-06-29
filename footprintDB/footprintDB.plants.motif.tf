822cc34a803bbda53c461b1883f651eac9da2409
Bruno Contreras <bconteras@eead.csic.es>
2016-06-29 09:22:45 +0200

    added Athaliana_cistrome

--- footprintDB/footprintDB.plants.motif.tf
+++ footprintDB/footprintDB.plants.motif.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc4e04a4c67f527beb7302e14f3b848ab51693bc17d1060d667032c97e1a740c
-size 1052110
+oid sha256:f10b3be37f9ab74a99c7c07948ed93ef96e88db575dad8d1248413b35445788f
+size 1863434
