230426f4db71b5de84a23ceb2900525320b4c065
RSAT admin at floresta plants.rsat.eu <bcontreras@eead.csic.es>
2016-06-29 09:36:33 +0200

    added Athaliana_cistrome

--- footprintDB/footprintDB.plants.motif.tf
+++ footprintDB/footprintDB.plants.motif.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a432a0060a5d1dc6902650d6dc900e02d3c1f6da5c2960f3adab150632855deb
-size 1060703
+oid sha256:f10b3be37f9ab74a99c7c07948ed93ef96e88db575dad8d1248413b35445788f
+size 1863434
