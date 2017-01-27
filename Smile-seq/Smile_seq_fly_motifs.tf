8c7a8df31af260b9994a063b2a0525b36c4f196f
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2017-01-27 20:54:48 +0100

    smile-seq motifs

---
+++ Smile-seq/Smile_seq_fly_motifs.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:765acd3aeb6c193d685e54202dc7288289f1b34f1e67f186da36143430e6d9cd
+size 8530
---
+++ Smile-seq/Smile_seq_human_motifs.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:655daea82c554df6cfeb89dd48893fb51c1e0ae8330febc913e4014d2b1a73f4
+size 51456
