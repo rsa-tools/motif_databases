b2cf2877a4da00b80fbac0ee86b6bb484e2fbe60
RSAT admin at floresta plants.rsat.eu <bcontreras@eead.csic.es>
2017-09-13 09:54:02 +0200

    updated after adding humanC2H2ZF-ChIP

--- footprintDB/footprintDB.motif.tf
+++ footprintDB/footprintDB.motif.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4d8667ec42aa87e55db5dabe16396a9aee12bd8dae1d053a113360626629509
+oid sha256:0f0c33665e37c69a561d17d630f44db7f7d8b2c733be4ded82fca456804fd1fa
 size 14901199
