538205944fa5c410a5e6e6e456fc0f656522c13c
RSAT admin at floresta plants.rsat.eu <bcontreras@eead.csic.es>
2017-09-13 09:54:02 +0200

    updated after adding humanC2H2ZF-ChIP

--- footprintDB/footprintDB.metazoa.motif.tf
+++ footprintDB/footprintDB.metazoa.motif.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c51d33cd1bac1f1f67d1a38ba8e63937263d620afa249d62318838824ebb8ca3
+oid sha256:812037f7065c27e0de613fd62a374ff7ce54557ed36f8fd41d7550eb7a484c6b
 size 9537617
