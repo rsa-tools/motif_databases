363ed2dcdf38a25c331aec0438ecc73be4690f6d
Thi-Thuy-Nga NGUYEN <thnguyen@ldogmac15.ens.fr>
2017-10-18 15:32:27 +0200

    Add RSAT nonredundant motifs

---
+++ RSAT_nonredundant_insects_2017.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d2943f3801125f52fe7f4cd5ccb2acdfdddc21b763e047253389b198eccc87e
+size 446675
---
+++ RSAT_nonredundant_plants_2017.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1e65b74111cb277db707ef7d4262423d88779e338d3c1fa0e2256b87ce43faa2
+size 450142
