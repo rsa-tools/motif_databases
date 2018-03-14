e57c90ee016551fccacaaebaee543a4df2828a46
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2018-03-14 13:54:39 +0100

    Clean IDs and ACs

--- ENCODE/encode-motifs_Kheradpour_Kellis_NAR2013.tf
+++ ENCODE/encode-motifs_Kheradpour_Kellis_NAR2013.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:613493997fe66666ec0c2c7af1b651c3436f34ca41549b8d0862ca527cd98bf8
-size 1530337
+oid sha256:fbc8e13404fa1c7d3e4b2fb6eb705b794a37f157b1b3e4b725a83e33c1290c32
+size 1530208
