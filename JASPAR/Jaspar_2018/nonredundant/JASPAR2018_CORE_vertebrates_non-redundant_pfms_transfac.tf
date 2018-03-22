72e3c1277459cd5d9412a97a1d1db8481aa99c7a
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2018-03-22 18:07:45 +0100

    Clean IDs and ACs

--- JASPAR/Jaspar_2018/nonredundant/JASPAR2018_CORE_vertebrates_non-redundant_pfms_transfac.tf
+++ JASPAR/Jaspar_2018/nonredundant/JASPAR2018_CORE_vertebrates_non-redundant_pfms_transfac.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3035a195c83abf910ceaa36d5b1d1f36fc46355a8b91e2ca4266d9ef6cc43719
+oid sha256:e5bebbedff526b130b8afe0e26d54b437d014c0441488fc3bedaecdf1f353542
 size 395009
