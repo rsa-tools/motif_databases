6b21d5ef2882e0f33b510d8dc33a5b1f01a34a56
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2018-03-22 18:07:37 +0100

    Clean IDs and ACs

--- JASPAR/Jaspar_2018/nonredundant/JASPAR2018_CORE_plants_non-redundant_pfms_transfac.tf
+++ JASPAR/Jaspar_2018/nonredundant/JASPAR2018_CORE_plants_non-redundant_pfms_transfac.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5effebd1f53cc0d779cec4399b7d388cbf74714b58ef3e5e77b51c69bc0e9c7a
+oid sha256:ebf3a157e8008f8740543b1eb8f870050bd3d384699c2d0f82697e47da3413bc
 size 346567
