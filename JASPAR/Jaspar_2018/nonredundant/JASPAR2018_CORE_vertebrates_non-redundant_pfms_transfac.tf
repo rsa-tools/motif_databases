81e5c37fe11ed16f190459479e1f5e12c416bf7b
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2018-03-29 12:12:24 +0200

    Renegerated from JASPAR -> Transfac conversion

--- JASPAR/Jaspar_2018/nonredundant/JASPAR2018_CORE_vertebrates_non-redundant_pfms_transfac.tf
+++ JASPAR/Jaspar_2018/nonredundant/JASPAR2018_CORE_vertebrates_non-redundant_pfms_transfac.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5bebbedff526b130b8afe0e26d54b437d014c0441488fc3bedaecdf1f353542
-size 395009
+oid sha256:f9282c124fbd9a5f53e6ef9d43cf545babeaefab88e45bd9b25c1450742e48b8
+size 413997
