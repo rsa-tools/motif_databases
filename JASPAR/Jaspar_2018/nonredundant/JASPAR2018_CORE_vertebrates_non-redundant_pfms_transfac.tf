2091d8f6a81592036fd7b2b3326ad234f8f67d68
Morgane <mthomas@biologie.ens.fr>
2018-03-29 16:19:09 +0200

    sorted fixed Jaspar vertebrate

--- JASPAR/Jaspar_2018/nonredundant/JASPAR2018_CORE_vertebrates_non-redundant_pfms_transfac.tf
+++ JASPAR/Jaspar_2018/nonredundant/JASPAR2018_CORE_vertebrates_non-redundant_pfms_transfac.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9282c124fbd9a5f53e6ef9d43cf545babeaefab88e45bd9b25c1450742e48b8
-size 413997
+oid sha256:185ccd7cfec9a0969638a88aa50a81883ba4b2757e84f7061190e2ce4a45d2b4
+size 402384
