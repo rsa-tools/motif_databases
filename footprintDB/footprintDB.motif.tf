a2b5c30e96494333c03246e4078e988e0fd77623
RSAT admin at floresta plants.rsat.eu <bcontreras@eead.csic.es>
2018-06-14 16:16:05 +0200

    Updated footprintDB motifs

--- footprintDB/footprintDB.motif.tf
+++ footprintDB/footprintDB.motif.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02c04ebd3cdc19fe781a56d95e503fbf87db4453db97f7bf798eb4fd5bc2a0de
-size 15254572
+oid sha256:f177fe19a777b148fbc466577c9d5558e4b9899396b1d7592c2e014ad2f07f07
+size 15634006
--- footprintDB/footprintDB.plants.motif.tf
+++ footprintDB/footprintDB.plants.motif.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7e308213a085d5db1a99c8eab2c2b7de1acf4450e2db6ef0f81409f9ad1e34c
-size 2361201
+oid sha256:807aff999c52599d1f71203894dc70a0c96a798f6bc1493c90c6f88c7855d3aa
+size 2422983
