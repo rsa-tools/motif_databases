a57d2f40d8f90b693698d42a44c309db0043b99b
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2018-09-25 13:48:53 +0200

    Novel motif databases for TFs interacting with/without nucleosomes

---
+++ NCAP-SELEX/CAP_SELEX_motifs.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:023c48c59ab3a20509899fefd175742acebb8c7693bd938efe8cc0ef07763aca
+size 87386
---
+++ NCAP-SELEX/NCAP_SELEX_motifs.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:caeb13bad2f97126556def343e3959730e514ddf0b15e7b474b032cf942276ee
+size 86692
