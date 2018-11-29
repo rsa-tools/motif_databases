e11eb22d591458e332af55d79c4d0f39b05407f5
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2018-11-29 14:06:58 +0100

    Added MethMotif databases, separated by cell types

---
+++ MethMotif_cell_types/HEK293T_MethMotif.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac58bd12d542355d9125eb6ceafed4cccdb92a72aa68df0a7282fa04a54cc230
+size 2581
---
+++ MethMotif_cell_types/IMR-90_MethMotif.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:48aea69b4ba740300bcc9e012435e12a6d16221b3dff118177d8c1a44bc90a73
+size 3808
