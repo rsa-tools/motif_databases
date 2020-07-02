295c203b7cc01b362f825a7f5eb9cc8ac26ad1d9
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2020-07-02 11:25:37 +0200

    Added Zinc finger motif collection from Dogan 2020

---
+++ Zinc_fingers_Dogan2020/Zinc_fingers_ChIP-exo.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff532e5a47f60a77570a23e5bf1c0e530d6cebac184b1d1d62d08de292115708
+size 176388
---
+++ Zinc_fingers_Dogan2020/Zinc_fingers_ChIP-seq.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:938882fccdabc4c0d9f30bbdf3497c6b2e69c25b6e33fd0cb0319cbb7596ae42
+size 148637
