76b34a01dcfd445896451a7835b4ce0dd01cb1ad
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2020-08-11 19:42:07 +0200

    Added TF names correctly

--- Zinc_fingers_Dogan2020/Zinc_fingers_ChIP-seq.tf
+++ Zinc_fingers_Dogan2020/Zinc_fingers_ChIP-seq.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:938882fccdabc4c0d9f30bbdf3497c6b2e69c25b6e33fd0cb0319cbb7596ae42
-size 148637
+oid sha256:7960d3d1623938540c663166ee08692fa967542967b072b7acd195d6b9724a18
+size 119060
