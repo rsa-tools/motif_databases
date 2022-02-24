83fde9e1f1266695d7cebf91e6284199a62f2da3
jvanheld <jacques.van-helden@univ-amu.fr>
2022-02-24 16:45:38 +0100

    Added JASPAR 2022 motifs: CORE + Unvalidated in the same file

---
+++ JASPAR/Jaspar_2022/JASPAR_2022_CORE_+_UNVALIDATED_fungi.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:75f16e6c0f2ec1f9f1b35992bed97d957b58be88151630e76488195d988378de
+size 114485
---
+++ JASPAR/Jaspar_2022/JASPAR_2022_CORE_+_UNVALIDATED_plants.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec5b2a2892528d6098f31f6286bdc393da8ac7493d7bf59e03e15711e6b88d84
+size 462167
