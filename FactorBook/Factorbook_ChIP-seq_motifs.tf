41e5549393101a01e03894d9335f6825b550de92
Bruno Contreras <bconteras@eead.csic.es>
2023-12-20 16:18:51 +0100

    truncated until correct files found

--- FactorBook/Factorbook_ChIP-seq_motifs.tf
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7d078fd20b44a8b2430857d6aac3b63eb8c6043181f3f238d22854d65cb46fed
-size 7897744
--- FactorBook/Factorbook_SELEX_motifs.tf
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ac83ecdcc6643e2cd6d6f5250007954c06431d433e4fb19dc3d39e036b90e2a5
-size 5727739
